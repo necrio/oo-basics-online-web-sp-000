841c604f4151fc157bd3c527ae6549f21c18b121
Keith Isham <kisham8899@gmail.com>
2019-05-04 15:40:15 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -40,19 +40,19 @@ end
 
 # class Book
 #   attr_accessor :author, :page_count, :genre
-#  
+#
 #   def initialize(title)
 #     @title = title
 #   end
-#  
+#
 #   def title
 #     @title
 #   end
-#  
+#
 #   def turn_page
 #     puts "Flipping the page...wow, you read fast!"
 #   end
-#  
+#
 # end
 
 book = Book.new("Some Title")
