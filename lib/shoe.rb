ca322ecac08f1e400aa68a925b45941657d8dfe5
Keith Isham <kisham8899@gmail.com>
2019-05-04 15:42:53 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,17 @@
 # Make your shoe class here!
+
+class Book
+  attr_accessor 
+# attr_reader :title
+
+  def initialize(title)
+  
+  end
+
+
+
+  # def turn_page
+  #   puts "Flipping the page...wow, you read fast!"
+  # end
+
+end
