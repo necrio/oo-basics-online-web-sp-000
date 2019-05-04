db103ecb35eb1d8e14a3bc62a4db87c784a303d7
Keith Isham <kisham8899@gmail.com>
2019-05-04 15:40:12 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -37,5 +37,23 @@ def page_count=(num)
 
 end
 
+
+# class Book
+#   attr_accessor :author, :page_count, :genre
+#  
+#   def initialize(title)
+#     @title = title
+#   end
+#  
+#   def title
+#     @title
+#   end
+#  
+#   def turn_page
+#     puts "Flipping the page...wow, you read fast!"
+#   end
+#  
+# end
+
 book = Book.new("Some Title")
 book.turn_page
